2a4a5af302cb603f876251d12aa4465f9a60004a
eli <eliitheboss@gmail.com>
2023-01-14 07:38:04 +0000

    completed a redirect to longURL if short was entered

--- express_server.js
+++ express_server.js
@@ -4,26 +4,48 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-function generateRandomString() {
-
-}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).slice(2);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]]
+  };
+
+  app.post("/urls", (req, res) => {
+    const longURLNew = req.body.longURL;
+    const shortURLId = generateRandomString();
+    urlDatabase[shortURLId] = longURLNew;
+
+    res.redirect(`/urls/${shortURLId}`);
+  });
+
+  app.get("/u/:id", (req, res) => {
+    const shortURLID = req.params.id;
+    const longURL = urlDatabase[shortURLID];
+
+    //edge case: client requests short URL with a non-existant id
+    if (!longURL) {
+      return res.status(404).send("Error: URL not found. Please enter valid id");
+    }
+
+    // redirect client to site
+    res.redirect(longURL);
+  });
+
   res.render("urls_index", templateVars);
 });
 
@@ -40,7 +62,10 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
   res.render("urls_show", templateVars);
 });
 
