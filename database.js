fc0336c58a32ebbd5a09a2893089a7cdcfcba025
eli <eliitheboss@gmail.com>
2023-01-18 10:44:40 +0000

    database and users object moved to module

---
+++ database.js
@@ -0,0 +1,27 @@
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "h67f5h"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "h67f5h"
+  },
+};
+
+// users
+const users = {
+  "h67f5h": {
+    id: "h67f5h",
+    email: "eli@gmail.com",
+    password: "1234"
+  },
+
+  "3g6j0s": {
+    id: "3g6j0s",
+    email: "jimmy@gmail.com",
+    password: "000"
+  }
+};
+
+module.exports = { urlDatabase, users }
